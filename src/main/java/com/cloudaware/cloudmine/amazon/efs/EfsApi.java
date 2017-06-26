42b838525c13ffdae8be79740ec283101b54ee4d
Ivan 'Mi0' Volkov <volkov@cloudaware.com>
2017-06-26 16:37:44 +0500

    AWS EFS api added

---
+++ src/main/java/com/cloudaware/cloudmine/amazon/efs/EfsApi.java
@@ -0,0 +1,125 @@
+package com.cloudaware.cloudmine.amazon.efs;
+
+import com.amazonaws.services.elasticfilesystem.model.DescribeFileSystemsRequest;
+import com.amazonaws.services.elasticfilesystem.model.DescribeFileSystemsResult;
+import com.amazonaws.services.elasticfilesystem.model.DescribeMountTargetSecurityGroupsRequest;
+import com.amazonaws.services.elasticfilesystem.model.DescribeMountTargetSecurityGroupsResult;
+import com.amazonaws.services.elasticfilesystem.model.DescribeMountTargetsRequest;
+import com.amazonaws.services.elasticfilesystem.model.DescribeMountTargetsResult;
+import com.amazonaws.services.elasticfilesystem.model.DescribeTagsRequest;
+import com.amazonaws.services.elasticfilesystem.model.DescribeTagsResult;
+import com.cloudaware.cloudmine.amazon.AmazonUnparsedException;
+import com.cloudaware.cloudmine.amazon.Constants;
+import com.google.api.server.spi.config.AnnotationBoolean;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
+
+@Api(
+        name = "efs",
+        canonicalName = "Efs",
+        title = "Amazon Elastic File System",
+        description = "Simple, scalable, and reliable file storage for the AWS Cloud",
+        namespace = @ApiNamespace(
+                ownerDomain = "cloudaware.com",
+                ownerName = "CloudAware",
+                packagePath = "cloudmine/amazon"
+        ),
+        version = "v1",
+        scopes = {Constants.EMAIL_SCOPE},
+        clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID},
+        apiKeyRequired = AnnotationBoolean.TRUE
+)
+public final class EfsApi {
+
+    @ApiMethod(
+            httpMethod = ApiMethod.HttpMethod.GET,
+            name = "fileSystems.list",
+            path = "{region}/file-systems"
+    )
+    public FileSystemsResponse fileSystemsList(
+            @Named("credentials") final String credentials,
+            @Named("region") final String region,
+            @Named("creationToken") @Nullable final String creationToken,
+            @Named("fileSystemId") @Nullable final String fileSystemId,
+            @Named("page") @Nullable final String page
+    ) throws AmazonUnparsedException {
+        return EfsCaller.get(DescribeFileSystemsRequest.class, FileSystemsResponse.class, credentials, region).execute((client, request, response) -> {
+            final DescribeFileSystemsResult result = client.describeFileSystems(
+                    request
+                            .withCreationToken(creationToken)
+                            .withFileSystemId(fileSystemId)
+                            .withMarker(page)
+            );
+            response.setFileSystems(result.getFileSystems());
+            response.setNextPage(result.getNextMarker());
+        });
+    }
+
+    @ApiMethod(
+            httpMethod = ApiMethod.HttpMethod.GET,
+            name = "mountTargets.list",
+            path = "{region}/mount-targets"
+    )
+    public MountTargetsResponse mountTargetsList(
+            @Named("credentials") final String credentials,
+            @Named("region") final String region,
+            @Named("fileSystemId") @Nullable final String fileSystemId,
+            @Named("mountTargetId") @Nullable final String mountTargetId,
+            @Named("page") @Nullable final String page
+    ) throws AmazonUnparsedException {
+        return EfsCaller.get(DescribeMountTargetsRequest.class, MountTargetsResponse.class, credentials, region).execute((client, request, response) -> {
+            final DescribeMountTargetsResult result = client.describeMountTargets(
+                    request
+                            .withFileSystemId(fileSystemId)
+                            .withMountTargetId(mountTargetId)
+                            .withMarker(page)
+            );
+            response.setMountTargets(result.getMountTargets());
+            response.setNextPage(result.getNextMarker());
+        });
+    }
+
+    @ApiMethod(
+            httpMethod = ApiMethod.HttpMethod.GET,
+            name = "mountTargets.securityGroups.list",
+            path = "{region}/mount-targets/{mountTargetId}/security-groups"
+    )
+    public SecurityGroupsResponse securityGroupsList(
+            @Named("credentials") final String credentials,
+            @Named("region") final String region,
+            @Named("mountTargetId") final String mountTargetId
+    ) throws AmazonUnparsedException {
+        return EfsCaller.get(DescribeMountTargetSecurityGroupsRequest.class, SecurityGroupsResponse.class, credentials, region).execute((client, request, response) -> {
+            final DescribeMountTargetSecurityGroupsResult result = client.describeMountTargetSecurityGroups(
+                    request
+                            .withMountTargetId(mountTargetId)
+            );
+            response.setSecurityGroups(result.getSecurityGroups());
+        });
+    }
+
+    @ApiMethod(
+            httpMethod = ApiMethod.HttpMethod.GET,
+            name = "fileSystems.tags.list",
+            path = "{region}/file-systems/{fileSystemId}/tags"
+    )
+    public TagsResponse tagsList(
+            @Named("credentials") final String credentials,
+            @Named("region") final String region,
+            @Named("fileSystemId") final String fileSystemId,
+            @Named("page") @Nullable final String page
+    ) throws AmazonUnparsedException {
+        return EfsCaller.get(DescribeTagsRequest.class, TagsResponse.class, credentials, region).execute((client, request, response) -> {
+            final DescribeTagsResult result = client.describeTags(
+                    request
+                            .withFileSystemId(fileSystemId)
+                            .withMarker(page)
+            );
+            response.setTags(result.getTags());
+            response.setNextPage(result.getNextMarker());
+        });
+    }
+}
